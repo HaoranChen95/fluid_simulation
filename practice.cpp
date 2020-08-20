841c340afcaeddd946c213ac64892d6b9d7e1eb7
HaoranChen95 <chen950302@live.com>
2020-08-20 16:47:00 +0200

    first commit

---
+++ practice.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void HelloWord();
+
+int main() 
+{
+    HelloWord();
+
+    return 0;
+
+}
+
+
+void HelloWord() //function to print "Hello word"
+{
+    cout << "Hello word!" << endl;
+
+}
