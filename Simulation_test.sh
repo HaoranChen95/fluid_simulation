9c5841c9735bdc35b0f618b092086ff78372e68b
ku702166 <ku702166@copy.hpc.itc.rwth-aachen.de>
2021-12-29 09:22:17 +0100

    rwth

--- Simulation_test.sh
+++ Simulation_test.sh
@@ -1,24 +1,36 @@
 #!/usr/bin/bash
 
-export OMP_NUM_THREADS=4
+#SBATCH --out=out_%j.txt
+#SBATCH --cpus-per-task=2
+#SBATCH --time=1-0
+
+module purge
+module load DEVELOP cmake/3.21.1 intel/19.1
+module list
+whereis cmake
+which cmake
+cmake --version
+
+
+export OMP_NUM_THREADS=2
 MD_time=1
 time_step=1e-4
 density=0.4
 kT=1.0
 gamma=0 # 0 MD >0 BD Simulation
 
+cd $HOME/fluid_simulation
+
+mkdir build
 cd build
-
 cmake ..
-
-make clean
 make
 
 pwd
-cp ./main ../temp_/.
-cp ../config/config.txt ../temp_/.
+cp ./main ../temp/.
+cp ../config/config.txt ../temp/.
 
-cd ../temp_
+cd ../temp
 mv main fluid_simulation
 rm -rf cfg* read* energy*
 
